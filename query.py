986e0da5f3861588320f8151d237551e83a8960e
Srikanth Manvi <srikanth.manvi@elastic.co>
2024-04-11 11:44:12 -0400

    Update query.py

--- query.py
+++ query.py
@@ -1,9 +1,10 @@
+# query.py
 from llama_index.core import VectorStoreIndex, QueryBundle, Response, Settings
 from llama_index.embeddings.ollama import OllamaEmbedding
 from llama_index.llms.ollama import Ollama
 from index import es_vector_store
-# Create index and query
 
+# Local LLM to send user query to
 local_llm = Ollama(model="mistral")
 Settings.embed_model= OllamaEmbedding("mistral")
 
@@ -11,7 +12,6 @@ index = VectorStoreIndex.from_vector_store(es_vector_store)
 query_engine = index.as_query_engine(local_llm, similarity_top_k=10)
 
 query="Give me summary of water related issues"
-bundle = QueryBundle(query,
-                    embedding=Settings.embed_model.get_query_embedding(query))
+bundle = QueryBundle(query, embedding=Settings.embed_model.get_query_embedding(query))
 result = query_engine.query(bundle)
 print(result)
