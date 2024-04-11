f52d3c51a3616a90ea3cd3cabbe6daff0e05eaf4
Srikanth Manvi <srikanth.manvi@elastic.co>
2024-04-11 11:43:56 -0400

    Update index.py

--- index.py
+++ index.py
@@ -1,3 +1,4 @@
+# index.py
 import json, os
 from llama_index.core import Document, Settings
 from llama_index.core.node_parser import SentenceSplitter
@@ -23,31 +24,37 @@ def get_documents_from_file(file):
 
 # Load .env file contents into env
 # ELASTIC_CLOUD_ID and ELASTIC_API_KEY are expected to be in the .env file.
-load_dotenv()
-
+load_dotenv('.env')
 
 # ElasticsearchStore is a VectorStore that
 # takes care of ES Index and Data management.
-es_vector_store = ElasticsearchStore(index_name="calls_123",
+es_vector_store = ElasticsearchStore(index_name="calls",
                                      vector_field='conversation_vector',
                                      text_field='conversation',
                                      es_cloud_id=os.getenv("ELASTIC_CLOUD_ID"),
                                      es_api_key=os.getenv("ELASTIC_API_KEY"))
 
-# Embedding Model to do local embedding using Ollama.
-# ollama_embedding = OllamaEmbedding("mistral")
-Settings.embed_model = OllamaEmbedding("mistral")
-# LlamaIndex Pipeline configured to take care of chunking, embedding
-# and storing the embeddings in the vector store.
-pipeline = IngestionPipeline(
-    transformations=[
-        SentenceSplitter(chunk_size=350, chunk_overlap=50),
-        OllamaEmbedding("mistral")
-    ], vector_store=es_vector_store
-)
 
-# Load data from a json file into a list of LlamaIndex Documents
-documents = get_documents_from_file(file="conversations.json")
+def main():
+    # Embedding Model to do local embedding using Ollama.
+    ollama_embedding = OllamaEmbedding("mistral")
 
-result = pipeline.run(documents=documents)
-print(".....Done running pipeline.....\n")
+    # LlamaIndex Pipeline configured to take care of chunking, embedding
+    # and storing the embeddings in the vector store.
+    pipeline = IngestionPipeline(
+        transformations=[
+            SentenceSplitter(chunk_size=350, chunk_overlap=50),
+            ollama_embedding,
+        ],
+        vector_store=es_vector_store
+    )
+
+    # Load data from a json file into a list of LlamaIndex Documents
+    documents = get_documents_from_file(file="conversations.json")
+
+    pipeline.run(documents=documents)
+    print(".....Done running pipeline.....\n")
+
+
+if __name__ == "__main__":
+    main()
